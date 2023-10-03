cf08df9cf038273b2b785bc653ae519c13ec100a
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-03 19:10:00 +0530

    created react

---
+++ apps/react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Calc from './Message'
+
+
+
+
+function App() {
+  return <Calc />
+}
+export default App
---
+++ apps/react-app/src/Message.tsx
@@ -0,0 +1,15 @@
+function Calc(){
+
+
+    return <div>
+        <label>enter first number</label>
+       <input type="number" id="first"></input>
+       <label>enter second number</label>
+       <input type="number" id="second"></input>
+       <button >add</button>
+       <button >sub</button>
+       <button >mul</button>
+       <button >div</button>
+    </div> 
+}
+ export default Calc;
